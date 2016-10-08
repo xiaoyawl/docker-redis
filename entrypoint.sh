e54a7779e778c002b993c85760aee7ce98fb292c
Legion-LookBack-Ben-Yoo <taoxiaoyuzy@vip.qq.com>
2016-10-09 00:02:45 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,7 +17,7 @@ fi
 
 # allow the container to be started with `--user`
 if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
-	chown -R redis ./redis
+	#chown -R redis ./redis
 	exec su-exec redis "$0" "$@"
 fi
 
